9a1f150eda5ff5ce84bd63885ef53bfdc5e027ca
Rafagg <rafael.gomez@gmail.com>
2018-02-24 17:53:22 +0100

    Poùulated with actual data

--- src/app/services/resume-data.service.ts
+++ src/app/services/resume-data.service.ts
@@ -29,14 +29,42 @@ export class ResumeDataService {
       ]
     };
     result.push(xp as Experience);
+    xp = {
+      company: 'Stendhal Museum Solutions, S.L.',
+      from: 'Jul 2011',
+      to: 'Feb 2012',
+      job: 'Software Architect',
+      highlights: [
+        'Redesign the architecture of the online ticket sales system.'
+      ]
+    };
     result.push(xp as Experience);
+    xp = {
+      company: 'Grupo Konecta',
+      from: 'Nov 2009',
+      to: 'Sep 2011',
+      job: 'Full-stack .NET developer',
+      highlights: [
+        'Developed software for the telemarketing and banking sector.',
+        'Technologies used: ASP.NET, Oracle, SQL Server'
+      ]
+    };
     result.push(xp as Experience);
+    xp = {
+      company: 'Coynet Systems SL',
+      from: 'Nov 2008',
+      to: 'Nov 2009',
+      job: '.NET Software developer',
+      highlights: [
+        'Developed software for a public rental Bicycle system',
+      ]
+    };
     result.push(xp as Experience);
 
     return Observable.of(result);
   }
 
-  public getSkills(): Observable<Skill[]>{
+  public getSkills(): Observable<Skill[]> {
     // TODO: In the future we will retrieve this info from an Firebase API
     const result: Skill[] = [];
     let sk: Skill = {name: 'Angular [2-5]', percentage: 90};
@@ -49,7 +77,9 @@ export class ResumeDataService {
     result.push(sk as Skill);
     sk = {name: 'GIT', percentage: 70};
     result.push(sk as Skill);
-
+    sk = {name: 'Linux', percentage: 60};
+    result.push(sk as Skill);
+    sk = {name: 'Real time systems', percentage: 60};
     result.push(sk as Skill);
     return Observable.of(result);
   }
